6db83345f8182532aba0c4e407b6723336ce6514
Healy <matt_healy@comcast.com>
2019-01-02 22:50:53 -0500

    cleaning up old files

--- Kronjob.py
+++
@@ -1,9 +0,0 @@
-from BigOlDB import BigOlDB
-from CoinClassifications import CoinClassifications
-import datetime, time
-from datetime import datetime
-
-BigOlDB = BigOlDB()
-CoinClassifications = CoinClassifications()
-
-BigOlDB.sqlUpdateMinutelyTable()
