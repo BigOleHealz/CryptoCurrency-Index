cecb311a972c0031ae5bd2ce01d9e85353539dae
Healy <matt_healy@comcast.com>
2019-01-25 16:58:24 -0500

    Trying to make rangeslider show only price

--- aggregator_functions.py
+++ aggregator_functions.py
@@ -1,24 +1,85 @@
 #!/usr/bin/env python3
 import pandas as pd
-import numpy as numpy
+import numpy as np
 import plotly.graph_objs as go
 from plotly import tools
-from styles import style_dicts
+from constants import style_dicts as styles, value_dicts as vals
 
-def movingAverage(values, window):
-	arr_avg = values.rolling(window).mean()
 
-	return arr_avg
+def refactor_to_ohlc(df,  currency_type, candlestick_window=50):
+	df['candle_num'] = df.index // candlestick_window
+	df['index'] = df.index
 
-##### SLOPPY #####
-def get_fig(df, currency_type, requested_mas, short_window=25, long_window=70):
+	dff = df.groupby('candle_num')['TimeStampID', 'Price_{}'.format(
+		currency_type)].max()
+	dff.rename(columns={
+		'TimeStampID': 'TimeStamp',
+		'Price_{}'.format(currency_type): 'High'},
+		inplace=True)
+	dff['Low'] = df.groupby('candle_num')['Price_{}'.format(currency_type)].min()
+	dff['Open'] = df.groupby('candle_num')['Price_{}'.format(currency_type)].first()
+	dff['Close'] = df.groupby('candle_num')['Price_{}'.format(currency_type)].last()
 
-	trace_price = go.Scatter(
+	return dff
+
+def rsi(prices, n=35):
+	deltas = np.diff(prices)
+	seed = deltas[:n+1]
+	up = seed[seed>=0].sum() / n
+	down = -seed[seed < 0].sum() / n
+	rs = up / down
+	rsi = np.zeros_like(prices)
+	rsi[:n] = 100.0 - (100.0 / (1.0 + rs))
+
+	for i in range(n, len(prices)):
+		delta = deltas[i-1]
+		if delta > 0:
+			upval = delta
+			downval = 0.0
+
+		else:
+			upval = 0.0
+			downval = -delta
+
+		up = (up * (n-1) + upval) / n
+		down = (down * (n-1) + downval) / n
+		rs = up/down
+		rsi[i] = 100.0 - 100.0/(1.0 + rs)
+
+	return rsi
+
+def get_fig(ticker, df, currency_type, requested_mas, candlestick, 
+			short_window=25, long_window=70):
+	
+	trace_rsi = go.Scatter(
+				x=df['TimeStampID'],
+				y=rsi(df['Price_{}'.format(currency_type)]),
+				mode='lines',
+				line={
+					'color': styles.colors['rsi'],
+					'width': styles.graph_layout['line_width']['rsi']
+					},
+				name='RSi',
+				yaxis='y',
+				)
+
+	if candlestick:
+		dff = refactor_to_ohlc(df, currency_type)
+		trace_price = go.Ohlc(x=dff['TimeStamp'],
+                open=dff['Open'],
+                high=dff['High'],
+                low=dff['Low'],
+                close=dff['Close'],
+                name='OHLC',
+				)
+
+	else:
+		trace_price = go.Scatter(
 				x=df['TimeStampID'],
 				y=df['Price_{}'.format(currency_type)],
 				mode='lines',
 				line={
-					'color': style_dicts.colors['current_price_line'],
+					'color': styles.colors['current_price_line'],
 					'width': 2.5
 					},
 				name='Price {}'.format(currency_type),
@@ -30,22 +91,20 @@ def get_fig(df, currency_type, requested_mas, short_window=25, long_window=70):
 				name='24hr Volume - USD',
 				fill='tozeroy',
 				line={
-					'color': style_dicts.colors['volume_line'],
+					'color': styles.colors['volume_line'],
 					'width': 1
 					},
-				fillcolor=style_dicts.colors['volume_fill'], #'rgba(26,150,65,0.5)'
-				# yaxis='y1'
+				fillcolor=styles.colors['volume_fill'], #'rgba(26,150,65,0.5)'
+				yaxis='y1'
 
 				)
 	
-	fig = tools.make_subplots(rows=3, cols=1, specs=[[{}], [{}], [{}]],
+	fig = tools.make_subplots(rows=4, cols=1, specs=[[{}], [{}], [{}], [{}]], 
                           shared_xaxes=True, shared_yaxes=False,
                           vertical_spacing=0.001)
+	fig.append_trace(trace_rsi, 1, 1)
 	fig.append_trace(trace_price, 2, 1)
 	fig.append_trace(trace_volume, 3, 1)
-
-	lay = fig['layout']
-	leg = lay['legend']
 
 	if 'sma' in requested_mas:
 		df['SMA'] = df['Price_{}'.format(currency_type)].rolling(short_window).mean()
@@ -54,12 +113,12 @@ def get_fig(df, currency_type, requested_mas, short_window=25, long_window=70):
 			y=df['SMA'],
 			mode='lines',
 			line={
-					'color': style_dicts.colors['sma_line'],
+					'color': styles.colors['sma_line'],
 					'width': 1
 					},
 			name='{}-Day Avg {}'.format(short_window, currency_type))
 		fig.append_trace(trace_sma, 2, 1)
-		
+
 	if 'lma' in requested_mas:
 		df['LMA'] = df['Price_{}'.format(currency_type)].rolling(long_window).mean()
 		trace_lma = go.Scatter(
@@ -67,7 +126,7 @@ def get_fig(df, currency_type, requested_mas, short_window=25, long_window=70):
 			y=df['LMA'],
 			mode='lines',
 			line={
-					'color': style_dicts.colors['lma_line'],
+					'color': styles.colors['lma_line'],
 					'width': 1
 					},
 			name='{}-Day Avg {}'.format(long_window, currency_type))
@@ -75,24 +134,34 @@ def get_fig(df, currency_type, requested_mas, short_window=25, long_window=70):
 
 	fig['layout'].update(title='Stacked Subplots with Shared X-Axes',
 		autosize=True,
-		paper_bgcolor=style_dicts.colors['transparent'],
-	    plot_bgcolor=style_dicts.colors['transparent'],
-	    titlefont={'color': style_dicts.colors['title_font']},
-	    legend={'bgcolor': style_dicts.colors['legend_bg']},
-	    margin=style_dicts.graph_layout['margins'],
+		paper_bgcolor=styles.base_colors['transparent'],
+	    plot_bgcolor=styles.base_colors['transparent'],
+	    titlefont={'color': styles.colors['title_font']},
+	    legend={'bgcolor': styles.colors['legend_bg']},
+	    margin=styles.graph_layout['margins'],
 	    grid={'rows': 5},
-	    xaxis={'color': style_dicts.base_colors['white'],
-	    	'tickangle': style_dicts.graph_layout['tickangle']},
-	    yaxis={'domain':[0, 0.3],
-	    	'color': style_dicts.base_colors['white'],
-	    	'tickangle': style_dicts.graph_layout['tickangle'],
-	    	'domain':[0, 0.3]
+	    xaxis={'color': styles.base_colors['white'],
+	    	'tickangle': styles.graph_layout['tickangle'],
+	    	'gridcolor': styles.base_colors['grey'],
+	    	'rangeslider': {'visible': True}},
+	    yaxis={'domain':[0, 1],
+	    	'color': styles.base_colors['white'],
+	    	'tickangle': styles.graph_layout['tickangle'],
+	    	'domain':[styles.graph_layout['domains']['y2_top'] + \
+	    		styles.graph_layout['domains']['padding'], 1]
 	    	# 'anchor': 'y2'
 	    	},
 	    yaxis2={
-	    	'color': style_dicts.base_colors['white'],
-	    	'tickangle': style_dicts.graph_layout['tickangle'],
-	    	'domain':[0, 1], 'anchor': 'y2'},
+	    	'color': styles.base_colors['white'],
+	    	'tickangle': styles.graph_layout['tickangle'],
+	    	'domain':[styles.graph_layout['domains']['y2_bottom'], 
+	    		styles.graph_layout['domains']['y2_top']], 'anchor': 'y2'},
+	    yaxis3={
+	    	'color': styles.base_colors['white'],
+	    	'tickangle': styles.graph_layout['tickangle'],
+	    	'domain':[0, styles.graph_layout['domains']['y2_bottom'] - \
+	    		styles.graph_layout['domains']['padding']], 
+	    	'anchor': 'y3'}
 	    ),
 
 
