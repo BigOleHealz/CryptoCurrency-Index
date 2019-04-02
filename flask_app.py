6f234a0e80562f3eb2b31a4fac9d3004fe1a3051
BigOleHealz <mth47@drexel.edu>
2019-04-02 18:56:26 -0400

    Flask Branch

--- flask_app.py
+++ flask_app.py
@@ -1,14 +1,33 @@
-from flask import Flask, render_template, url_for, flash, redirect
+from flask import Flask, render_template, url_for, flash, redirect, session, g
 from forms import RegistrationForm, LoginForm
 from big_ol_db import BigOlDB
+from flask_nav import Nav
+from flask_nav.elements import Navbar, Subgroup, View, Link, Separator, Text
 from datetime import datetime
 import pymysql, hashlib, pandas as pd, logging, static.flash_messages as fl_mes
+import random
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = '8b0f39878e8d95081dff1bb8e35b526a'
+nav = Nav(app)
+
+nav.register_element('my_navbar', Navbar('thenav',
+	View('Home Page', 'index'),
+	View('Item One', 'item', item=1),
+	Link('Google', 'https://www.google.com'),
+	Separator(),
+	Text('Here is some Text'),
+	Subgroup('Extras',
+		Link('yahoo', 'https://www.yahoo.com'),
+		View('Index', 'index'))
+		))
+
+
+app.secret_key = '\xfd{H\xe5<\x95\xf9\xe3\x96.5\xd1\x01O<!\xd5\xa2\xa0\x9fR"\xa1\xa8'
 
 bodb = BigOlDB()
+
+
 
 
 @app.route("/")
@@ -16,17 +35,67 @@ bodb = BigOlDB()
 def home():
     return render_template('home.html')
 
-
 @app.route("/about")
 def about():
     return render_template('about.html', title='about')
+
+@app.route('/')
+def index():
+	return render_template('index.html')
+
+@app.route('/items/<item>')
+def item(item):
+	return '<h1>THE ITEM PAGE!!! THE ITEM IS: {}.'.format(item)
+
+# @app.before_request
+# def before_request():
+# 	g.user = None
+# 	if "user" in session:
+# 		g.user = session["user"]
+
+
+
+# @app.route("/", methods=["GET", "POST"])
+# def index():
+# 	if request.method == "POST":
+# 		session.pop("user", None)
+# 		if request.form["password"] == "password":
+# 			session['user'] = request.form["username"]
+# 			return redirect(url_for("protected"))
+# 	return render_template('index.html')
+
+# @app.route("/protected")
+# def protected():
+# 	return render_template("protected.html")
+
+# @app.route("/getsession")
+# def get_session():
+# 	if "user" in session:
+# 		return session['user']
+# 	return "Not logged in!"
+
+# @app.route("/dropsession")
+# def drop_session():
+# 	session.pop("user", None)
+# 	return "Dropped!"
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
 	form = RegistrationForm()
 	if form.validate_on_submit():
-		
 		existing_account = bodb.existing_account(form.email.data, form.username.data)
 		if existing_account == False:
 			registered_status = bodb.register_user(form.email.data, form.username.data,
@@ -49,6 +118,8 @@ def login():
 	form = LoginForm()
 	if form.validate_on_submit():
 		validated = bodb.validate_user(form.email.data, form.password.data)
+		
+
 		if validated == True:
 			return redirect(url_for('home'))
 		else:
