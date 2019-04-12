266b40ae4c4ca61b36000032f498ae45272fe2ff
BigOleHealz <mth47@drexel.edu>
2019-04-11 22:06:21 -0400

    Interactive Flask Skeleton

--- flask_app.py
+++ flask_app.py
@@ -1,88 +1,86 @@
-from flask import Flask, render_template, url_for, flash, redirect, session, g
+from flask import Flask, render_template, url_for, flash, redirect, request
 from forms import RegistrationForm, LoginForm
 from big_ol_db import BigOlDB
 from flask_nav import Nav
 from flask_nav.elements import Navbar, Subgroup, View, Link, Separator, Text
 from datetime import datetime
 import pymysql, hashlib, pandas as pd, logging, static.flash_messages as fl_mes
-import random
+
+import numpy as np, json, plotly
+import plotly.graph_objs as go
+
 
 app = Flask(__name__)
 
-nav = Nav(app)
-
-nav.register_element('my_navbar', Navbar('thenav',
-	View('Home Page', 'index'),
-	View('Item One', 'item', item=1),
-	Link('Google', 'https://www.google.com'),
-	Separator(),
-	Text('Here is some Text'),
-	Subgroup('Extras',
-		Link('yahoo', 'https://www.yahoo.com'),
-		View('Index', 'index'))
-		))
 
 
 app.secret_key = '\xfd{H\xe5<\x95\xf9\xe3\x96.5\xd1\x01O<!\xd5\xa2\xa0\x9fR"\xa1\xa8'
 
 bodb = BigOlDB()
 
+
+
+
+
+
+
+
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('home.html')
+	return render_template('home.html')
 
 @app.route("/about")
 def about():
-    return render_template('about.html', title='about')
+	return render_template('about.html', title='about')
 
-@app.route('/')
+
+
+
+
+
+@app.route('/index')
 def index():
-	return render_template('index.html')
+    feature = 'Bar'
+    bar = create_plot(feature)
+    return render_template('index.html', plot=bar)
 
-@app.route('/items/<item>')
-def item(item):
-	return '<h1>THE ITEM PAGE!!! THE ITEM IS: {}.'.format(item)
+def create_plot(feature):
+    if feature == 'Bar':
+        N = 40
+        x = np.linspace(0, 1, N)
+        y = np.random.randn(N)
+        df = pd.DataFrame({'x': x, 'y': y}) # creating a sample dataframe
+        data = [
+            go.Bar(
+                x=df['x'], # assign x as the dataframe column 'x'
+                y=df['y']
+            )
+        ]
+    else:
+        N = 1000
+        random_x = np.random.randn(N)
+        random_y = np.random.randn(N)
 
-# @app.before_request
-# def before_request():
-# 	g.user = None
-# 	if "user" in session:
-# 		g.user = session["user"]
-
-
-
-# @app.route("/", methods=["GET", "POST"])
-# def index():
-# 	if request.method == "POST":
-# 		session.pop("user", None)
-# 		if request.form["password"] == "password":
-# 			session['user'] = request.form["username"]
-# 			return redirect(url_for("protected"))
-# 	return render_template('index.html')
-
-# @app.route("/protected")
-# def protected():
-# 	return render_template("protected.html")
-
-# @app.route("/getsession")
-# def get_session():
-# 	if "user" in session:
-# 		return session['user']
-# 	return "Not logged in!"
-
-# @app.route("/dropsession")
-# def drop_session():
-# 	session.pop("user", None)
-# 	return "Dropped!"
+        # Create a trace
+        data = [go.Scatter(
+            x = random_x,
+            y = random_y,
+            mode = 'markers'
+        )]
 
 
+    graphJSON = json.dumps(data, cls=plotly.utils.PlotlyJSONEncoder)
 
+    return graphJSON
 
+@app.route('/bar', methods=['GET', 'POST'])
+def change_features():
 
+    feature = request.args['selected']
+    graphJSON= create_plot(feature)
 
-
-
+    return graphJSON
 
 
 
@@ -125,5 +123,5 @@ def login():
 
 
 if __name__ == "__main__":
-	app.run(debug=True)
+	app.run(debug=True, port=8050)
 
